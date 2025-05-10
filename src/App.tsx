4e716edcc1b4a184f6cbced6306fba854594a2a2
ViktorLaishev <laishevviktor@gmail.com>
2025-05-10 15:06:38 +0000

    Fix: Wrap EventLog with PetriNetProvider
    The error "usePetriNet must be used within a PetriNetProvider" indicates that the EventLog component is using the usePetriNet hook without being wrapped by the PetriNetProvider. This commit wraps the EventLog component with the provider to resolve the error.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import EventLog from "./pages/EventLog";
 import NotFound from "./pages/NotFound";
+import { PetriNetProvider } from "./contexts/PetriNetContext";
 
 const queryClient = new QueryClient();
 
@@ -18,7 +19,14 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/event-log" element={<EventLog />} />
+          <Route 
+            path="/event-log" 
+            element={
+              <PetriNetProvider>
+                <EventLog />
+              </PetriNetProvider>
+            } 
+          />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
