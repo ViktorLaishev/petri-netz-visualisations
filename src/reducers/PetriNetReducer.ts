95b4ea8583db92770a08083077626f5acd96f9a5
ViktorLaishev <laishevviktor@gmail.com>
2025-05-11 14:32:19 +0000

    Reverted to commit da5ee6f558f4ff0e7189d01a66d9aaf45c37bc04
    Co-authored-by: mremillime <emil.mckarov@gmail.com>

--- src/reducers/PetriNetReducer.ts
+++
@@ -1,8 +0,0 @@
-
-// Add this case to the reducer
-case 'LOAD_HISTORICAL_STATE':
-  return {
-    ...state,
-    graph: action.payload.graph,
-    currentHistoryIndex: action.payload.historyIndex
-  };
